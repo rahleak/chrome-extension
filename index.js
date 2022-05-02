9fb4edf4ab46bb7540934c44aa1bb068905d9a89
Rahwa Leakemariam <rah.leak@gmail.com>
2022-05-02 17:13:00 -0400

    inital commit. js, html, css files created. eventListener added to button to save leads.

---
+++ index.js
@@ -0,0 +1,13 @@
+let myLeads = [];
+const inputBtn = document.getElementById("input-btn");
+const inputEl = document.getElementById("input-el");
+const ulEL = document.getElementById("ul-el");
+
+inputBtn.addEventListener("click", function(){
+  myLeads.push(inputEl.value);
+  console.log(myLeads)
+})
+
+for (let i = 0; i < myLeads.length; i++) {
+  console.log(myLeads[i])
+}
