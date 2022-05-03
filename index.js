b2475c6d9b3627cbea499c417b1ee9aadaae6de9
Rahwa Leakemariam <rah.leak@gmail.com>
2022-05-03 17:33:17 -0400

    lists render via button. input filed clears. links direct user to appropriate site on a new tab

--- index.js
+++ index.js
@@ -3,11 +3,34 @@ const inputBtn = document.getElementById("input-btn");
 const inputEl = document.getElementById("input-el");
 const ulEL = document.getElementById("ul-el");
 
+
+
+function renderLeads() {
+  
+  let listItems = ""
+  
+  for (let lead of myLeads) {
+    
+    listItems += `
+    <li> 
+      <a target="_blank" href=${lead} > 
+        ${lead} 
+      </a> 
+    </li>
+    `
+    
+    
+    // const li = document.createElement("li");
+    // li.textContent += lead;
+    // ulEL.append(li);
+  }
+  
+  ulEL.innerHTML = listItems
+}
+
 inputBtn.addEventListener("click", function(){
   myLeads.push(inputEl.value);
-  console.log(myLeads)
-})
-
-for (let i = 0; i < myLeads.length; i++) {
-  console.log(myLeads[i])
-}
+  inputEl.value = ""
+    renderLeads();
+    console.log(myLeads)
+  })
